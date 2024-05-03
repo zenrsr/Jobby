3f197402d36b7c18c92df9f363d51825e72d12f5
zenrsr <ragasandeepreddy25@gmail.com>
2024-05-03 17:02:30 +0000

    Initial Commit

---
+++ src/components/AllJobs/index.js
@@ -0,0 +1,83 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import Cookies from 'js-cookie'
+import {AiOutlineSearch} from 'react-icons/ai'
+import Header from '../Header'
+import JobItem from '../JobItem'
+import './index.css'
+
+const employementTypesList = [
+  {label: 'Full Time', employmentTypeId: 'FULLTIME'},
+  {label: 'Part Time', employmentTypeId: 'PARTTIME'},
+  {label: 'Freelance', employmentTypeId: 'FREELANCE'},
+  {label: 'Internship', employmentTypeId: 'INTERNSHIP'},
+]
+
+const salaryRangeList = [
+  {salaryRangeId: '1000000', label: '10 LPA and above'},
+  {salaryRangeId: '2000000', label: '20 LPA and above'},
+  {salaryRangeId: '3000000', label: '30 LPA and above'},
+  {salaryRangeId: '4000000', label: '40 LPA and above'},
+]
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+const apiJobStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+const failureViewImg = 'http://assets.ccbp.in/frontend/react-js/failure-img.png'
+class AllJobs extends Component {
+  state = {
+    profileData: [],
+    jobsData: [],
+    checkboxInputs: [],
+    radioInput: '',
+    searchInput: '',
+    apiStatus: apiStatusConstants.initial,
+    apiJobStatus: apiJobStatusConstants.initial,
+  }
+
+  componentDidMount() {
+    this.onGetProfileDetails()
+    this.onGetJobDetails()
+  }
+
+  onGetProfileDetails = async () => {
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+    const jwtToken = Cookies.get('jwt_token')
+    const {checkboxInputs, radioInput, searchInput} = this.state
+    const profileUrl = 'http://apis.ccbp.in/profile'
+    const optionsProfile = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const responsibleProfile = await fetch(profileUrl, optionsProfile)
+
+    if (responsibleProfile.ok === true) {
+      const fetchedDataProfile = [await responsibleProfile.json()]
+      const updatedDataProfile = fetchedDataProfile.map(eachItem => ({
+        name: eachItem.profile_details.name,
+        profileImageUrl: eachItem.profile_details.profile_image_url,
+        shortBio: eachItem.profile_details.short_bio,
+      }))
+      this.setState({
+        profileData: updatedDataProfile,
+        responseSuccess: true,
+        apiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({
+        apiStatus: apiStatusConstants.failure,
+      })
+    }
+  }
+}
